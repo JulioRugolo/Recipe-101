0f19a6ec781996bcecbc6f9c245048bb27824d98
JulioRugolo <juliorugolo@gmail.com>
2023-05-10 17:34:52 -0300

    Fix: Tests

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,7 +5,10 @@ import { act } from 'react-dom/test-utils';
 import App from '../App';
 import userTest from './mocks/user';
 import { renderWithRouter } from './helpers/renderWithRouter';
+import Profile from '../pages/Profile';
 // import Recipes from '../pages/Recipes';
+import pathFunc from './helpers/pathFunc';
+import AppProvider from '../context/AppProvider';
 
 describe('Testa a página Login', () => {
   test('Testa se o botão começa desabilitado e fica habilitado após preencher o form', () => {
@@ -31,6 +34,19 @@ describe('Testa a página Login', () => {
   });
   test('testa o component header na pagina meals', () => {
     renderWithRouter(<App />);
+    const emailInput = screen.getByTestId('email-input');
+    expect(emailInput).toBeInTheDocument();
+    const passwordInput = screen.getByTestId('password-input');
+    expect(passwordInput).toBeInTheDocument();
+
+    const submitButton = screen.getByTestId('login-submit-btn');
+    expect(submitButton).toBeInTheDocument();
+    expect(submitButton).toBeDisabled();
+
+    userEvent.type(emailInput, userTest.email);
+    userEvent.type(passwordInput, userTest.password);
+    expect(submitButton.disabled).toBe(false);
+    userEvent.click(submitButton);
     const profileButton = screen.getByTestId('profile-top-btn');
     const searchButton = screen.getByTestId('search-top-btn');
     expect(profileButton).toBeInTheDocument();
@@ -38,13 +54,27 @@ describe('Testa a página Login', () => {
   });
   test('testa se ao clicar no botão de search, o input search é renderizado na tela', () => {
     renderWithRouter(<App />);
+    const emailInput = screen.getByTestId('email-input');
+    expect(emailInput).toBeInTheDocument();
+    const passwordInput = screen.getByTestId('password-input');
+    expect(passwordInput).toBeInTheDocument();
+
+    const submitButton = screen.getByTestId('login-submit-btn');
+    expect(submitButton).toBeInTheDocument();
+    expect(submitButton).toBeDisabled();
+
+    userEvent.type(emailInput, userTest.email);
+    userEvent.type(passwordInput, userTest.password);
+    expect(submitButton.disabled).toBe(false);
+    userEvent.click(submitButton);
     const searchButton = screen.getByTestId('search-top-btn');
     userEvent.click(searchButton);
     const searchInput = screen.getByTestId('search-input');
     expect(searchInput).toBeInTheDocument();
   });
   test('testa se ao clicar no botão de profile, a pagina Profile é renderizada', () => {
-    const { history } = renderWithRouter(<App />);
+    const { history } = renderWithRouter(<AppProvider><App /></AppProvider>);
+    pathFunc(history, '/meals');
     const profileButton = screen.getByTestId('profile-top-btn');
     userEvent.click(profileButton);
     act(() => {
