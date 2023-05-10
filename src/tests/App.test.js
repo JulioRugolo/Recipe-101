6b9307398616fe4c14c7969985d879296c0993ea
JulioRugolo <juliorugolo@gmail.com>
2023-05-10 20:05:58 -0300

    fix: lint

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,20 +5,23 @@ import { act } from 'react-dom/test-utils';
 import App from '../App';
 import userTest from './mocks/user';
 import { renderWithRouter } from './helpers/renderWithRouter';
-import Profile from '../pages/Profile';
 // import Recipes from '../pages/Recipes';
 import pathFunc from './helpers/pathFunc';
 import AppProvider from '../context/AppProvider';
 
+const emailTestId = 'email-input';
+const passTestId = 'password-input';
+const loginTestId = 'login-submit-btn';
+
 describe('Testa a página Login', () => {
   test('Testa se o botão começa desabilitado e fica habilitado após preencher o form', () => {
     const { history } = renderWithRouter(<App />);
-    const emailInput = screen.getByTestId('email-input');
+    const emailInput = screen.getByTestId(emailTestId);
     expect(emailInput).toBeInTheDocument();
-    const passwordInput = screen.getByTestId('password-input');
+    const passwordInput = screen.getByTestId(passTestId);
     expect(passwordInput).toBeInTheDocument();
 
-    const submitButton = screen.getByTestId('login-submit-btn');
+    const submitButton = screen.getByTestId(loginTestId);
     expect(submitButton).toBeInTheDocument();
     expect(submitButton).toBeDisabled();
 
@@ -34,12 +37,12 @@ describe('Testa a página Login', () => {
   });
   test('testa o component header na pagina meals', () => {
     renderWithRouter(<App />);
-    const emailInput = screen.getByTestId('email-input');
+    const emailInput = screen.getByTestId(emailTestId);
     expect(emailInput).toBeInTheDocument();
-    const passwordInput = screen.getByTestId('password-input');
+    const passwordInput = screen.getByTestId(passTestId);
     expect(passwordInput).toBeInTheDocument();
 
-    const submitButton = screen.getByTestId('login-submit-btn');
+    const submitButton = screen.getByTestId(loginTestId);
     expect(submitButton).toBeInTheDocument();
     expect(submitButton).toBeDisabled();
 
@@ -54,9 +57,9 @@ describe('Testa a página Login', () => {
   });
   test('testa se ao clicar no botão de search, o input search é renderizado na tela', () => {
     renderWithRouter(<App />);
-    const emailInput = screen.getByTestId('email-input');
+    const emailInput = screen.getByTestId(emailTestId);
     expect(emailInput).toBeInTheDocument();
-    const passwordInput = screen.getByTestId('password-input');
+    const passwordInput = screen.getByTestId(passTestId);
     expect(passwordInput).toBeInTheDocument();
 
     const submitButton = screen.getByTestId('login-submit-btn');
