b4bdbc32c300110d8f29b8af52597243e2cf62a5
JulioRugolo <juliorugolo@gmail.com>
2023-05-11 22:29:34 -0300

    feat: req 48 e 49

--- src/pages/DoneRecipePage.js
+++ src/pages/DoneRecipePage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import Header from '../components/Header';
 import AppContext from '../context/AppContext';
 import DoneRecipeMeals from '../components/DoneRecipeMeals';
@@ -8,32 +8,47 @@ function DoneRecipePage() {
   const { setTitle, setFavoriteOrProfile } = useContext(AppContext);
   const DONE_RECIPES = 'doneRecipes';
   const data = JSON.parse(localStorage.getItem(DONE_RECIPES));
+  const [dataFiltered, setDataFiltered] = useState(data);
   useEffect(() => {
     setTitle('Done Recipes');
     setFavoriteOrProfile(true);
   });
+  const handleClick = ({ target }) => {
+    if (target.innerHTML === 'Meals') {
+      const mealsArray = data.filter((recipe) => recipe.type === 'meal');
+      setDataFiltered(mealsArray);
+    } else if (target.innerHTML === 'Drinks') {
+      const drinksArray = data.filter((recipe) => recipe.type === 'drink');
+      setDataFiltered(drinksArray);
+    } else {
+      setDataFiltered(data);
+    }
+  };
   return (
     <>
       <Header />
       <button
-        data-testid="filter-by-all-btn"
-      >
-        All
-
-      </button>
-      <button
         data-testid="filter-by-meal-btn"
+        onClick={ (event) => handleClick(event) }
       >
         Meals
 
       </button>
       <button
         data-testid="filter-by-drink-btn"
+        onClick={ (event) => handleClick(event) }
       >
         Drinks
 
       </button>
-      {data && data.map((recipe, index) => {
+      <button
+        data-testid="filter-by-all-btn"
+        onClick={ (event) => handleClick(event) }
+      >
+        All
+
+      </button>
+      {dataFiltered && dataFiltered.map((recipe, index) => {
         if (recipe.type === 'meal') {
           return <DoneRecipeMeals recipe={ recipe } index={ index } key={ recipe.id } />;
         }
